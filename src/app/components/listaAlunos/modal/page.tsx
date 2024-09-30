2b04ab709cdf5dd9d38211c3d5ef6bd27bcb6834
ArtCM <arthurperfilprof@hotmail.com>
2024-09-30 06:49:53 -0300

    fix: ajuste para deploy

--- src/app/components/listaAlunos/modal/page.tsx
+++ src/app/components/listaAlunos/modal/page.tsx
@@ -1,8 +1,7 @@
-
 'use client'
 
 import "./style.css"
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 
 interface AdicionarAlunoModalProps {
   isOpen: boolean;
@@ -16,20 +15,21 @@ export default function AdicionarAlunoModal({ isOpen, onClose, onSubmit }: Adici
   const [notas, setNotas] = useState<number[]>(Array(5).fill(0));
   const [frequencia, setFrequencia] = useState<number>(0);
 
-  const handleOutsideClick = (event: MouseEvent) => {
+  const handleOutsideClick = useCallback((event: MouseEvent) => {
     if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
       onClose();
     }
-  };
+  }, [onClose]);
 
   useEffect(() => {
     if (isOpen) {
       document.addEventListener('mousedown', handleOutsideClick);
     }
+
     return () => {
       document.removeEventListener('mousedown', handleOutsideClick);
     };
-  }, [isOpen]);
+  }, [isOpen, handleOutsideClick]);
 
   const handleNotaChange = (index: number, value: string) => {
     const updatedNotas = [...notas];
@@ -40,60 +40,60 @@ export default function AdicionarAlunoModal({ isOpen, onClose, onSubmit }: Adici
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     onSubmit({ nome, notas, frequencia });
-    setNome(''); 
+    setNome('');
     setNotas(Array(5).fill(0));
-    setFrequencia(0); 
+    setFrequencia(0);
   };
 
   if (!isOpen) return null;
 
-    return(
-        <div className="adicionarAlunoModal">
-            <div className="adicionarAlunoModal__modal" ref={modalRef}>
-                <button onClick={onClose} className="modal-close">
-                    X
-                </button>
-                <h2>Adicionar Aluno</h2>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Nome:
-                        <input
-                        type="text"
-                        value={nome}
-                        onChange={(e) => setNome(e.target.value)}
-                        required
-                        />
-                    </label>
+  return (
+    <div className="adicionarAlunoModal">
+      <div className="adicionarAlunoModal__modal" ref={modalRef}>
+        <button onClick={onClose} className="modal-close">
+          X
+        </button>
+        <h2>Adicionar Aluno</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              required
+            />
+          </label>
 
-                    {notas.map((nota, index) => (
-                        <label key={index}>
-                        Nota {index + 1}:
-                        <input
-                            type="number"
-                            value={nota}
-                            onChange={(e) => handleNotaChange(index, e.target.value)}
-                            required
-                            min="0"
-                            max="10"
-                        />
-                        </label>
-                    ))}
+          {notas.map((nota, index) => (
+            <label key={index}>
+              Nota {index + 1}:
+              <input
+                type="number"
+                value={nota}
+                onChange={(e) => handleNotaChange(index, e.target.value)}
+                required
+                min="0"
+                max="10"
+              />
+            </label>
+          ))}
 
-                    <label>
-                        Frequência (%):
-                        <input
-                        type="number"
-                        value={frequencia}
-                        onChange={(e) => setFrequencia(parseFloat(e.target.value))}
-                        required
-                        min="0"
-                        max="100"
-                        step="0.01"
-                        />
-                    </label>
-                    <button type="submit">Enviar</button>
-                </form>
-            </div>
-        </div>
-    );
+          <label>
+            Frequência (%):
+            <input
+              type="number"
+              value={frequencia}
+              onChange={(e) => setFrequencia(parseFloat(e.target.value))}
+              required
+              min="0"
+              max="100"
+              step="0.01"
+            />
+          </label>
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </div>
+  );
 }
